0ce268408452b8bb5ae6f66d8e2d703254b98df4
GuneyCaglayan <gnycglyn@hotmail.com>
2020-08-29 16:34:19 +0300

    Initial Commit

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $(document).on("click", ".image-wrapper", function () {
+        $(this).closest(".item-wrapper").addClass("opened");
+    });
+
+    $(document).on("click", ".return-menu", function (e) {
+        e.preventDefault();
+        $(this).closest(".item-wrapper").removeClass("opened");
+    })
+});
---
+++ js/reklam.js
@@ -0,0 +1,10 @@
+setTimeout("fonksiyon()",7000);  //7 saniye = 7000 milisaniye
+
+function gizle(){
+
+
+    document.getElementById("reklam").style.visibility="hidden";
+
+}
+
+window.setTimeout("gizle()",7000);
